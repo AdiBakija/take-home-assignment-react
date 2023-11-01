d220b99c7489b73c84a49b9e40584449f8bafc9c
Adi Bakija <adibakija35@hotmail.com>
2023-11-01 17:19:27 -0400

    Created route components; Setup basic login logic; Configured Apollo client to handle authorization better

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
     uri: 'http://localhost:8080/graphql/',
+})
+
+const authLink = setContext((_, { headers }) => {
+    // Should add the expiry + refresh logic here
+    const token = '' // Use the access token saved in localStorage here
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
---
+++ app/src/routes/Login.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import Cookies from 'js-cookie'
+
+const LOGIN_MUTATION = gql`
+    mutation Authenticate($email: String!, $password: String!) {
+        authenticate(email: $email, password: $password) {
+            accessToken
+            refreshToken
+            expiresAt
+        }
+    }
+`
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [login, { error, loading }] = useMutation(LOGIN_MUTATION)
+
+    const handleLogin = async () => {
+        try {
+            const { data } = await login({
+                variables: { email, password },
+            })
+            console.log('Data', data)
+            const accessToken = data.authenticate.accessToken
+            const refreshToken = data.authenticate.refreshToken
+            const expiresAt = data.authenticate.expiresAt
+
+            // Save session to cookies to persist user
+            Cookies.set(
+                'accessToken',
+                accessToken,
+                {
+                    expires: new Date(expiresAt),
+                    sameSite: 'strict',
+                }
+            )
+            Cookies.set(
+                'refreshToken',
+                refreshToken,
+                {
+                    sameSite: 'strict',
+                }
+            )
+        } catch (error) {
+            console.error('Login failed:', error)
+        }
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button onClick={handleLogin} disabled={loading}>
+                {loading ? 'Logging In...' : 'Log In'}
+            </button>
+            {error && <p>Error: {error.message}</p>}
+        </div>
+    )
+}
+
+export default Login
