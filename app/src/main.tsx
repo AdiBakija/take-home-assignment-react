0a8aed87ee43fa126f0dfc7000097ce95e8e5965
Adi Bakija <adibakija35@hotmail.com>
2023-11-01 14:01:40 -0400

    Added new packages with basic configuration for Apollo, Gql, React router and tailwind

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+const client = new ApolloClient({
+    uri: 'http://localhost:8080/graphql/',
+    cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        </BrowserRouter>
     </React.StrictMode>
 )
