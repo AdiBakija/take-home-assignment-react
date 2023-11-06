bfe47bda3f7200fb36386728fb8cec04946a971b
Adi Bakija <adibakija35@hotmail.com>
2023-11-06 16:23:07 -0500

    Changed apollo client to use HTTP request for refreshing token; Updated server side logic to whitelist incoming requests from 3000 preventing CORS issues

--- app/src/client/client.ts
+++ app/src/client/client.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink, gql } from '@apollo/client'
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink, gql, Observable } from '@apollo/client'
 import { getAuthCookies, setAuthCookies } from '../utils/auth.js'
 import { setContext } from '@apollo/client/link/context'
 import { onError } from "@apollo/client/link/error"
@@ -8,26 +8,34 @@ const refreshToken = async () => {
 
     try {
         let newAccessToken
-        client.mutate(
-            {
-                mutation: gql`
-                mutation RefreshSession($accessToken: String!, $refreshToken: String!) {
-                    refreshSession(accessToken: $accessToken, refreshToken: $refreshToken) {
-                      accessToken
-                      refreshToken
-                      expiresAt
-                    }
-                  }
-                  `,
-                variables: {
-                    accessToken: accessToken,
-                    refreshToken: refreshToken,
-                }
+
+        const url = 'http://localhost:8080/api/refresh'
+        const contentType = 'application/json'
+        const requestBody = {
+            accessToken,
+            refreshToken,
+        }
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': contentType,
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(requestBody),
+        }
+        await fetch(url, requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error(`Request failed with status ${response.status}`)
             }
-        ).then((result) => {
-            newAccessToken = result.data.refreshSession.accessToken
-            const newRefreshToken = result.data.refreshSession.refreshToken
-            const newExpiresAt = result.data.refreshSession.expiresAt
+        })
+        .then((result) => {
+            // Handle the response data, which should contain accessToken, refreshToken, and expiresAt
+            newAccessToken = result.accessToken
+            const newRefreshToken = result.refreshToken
+            const newExpiresAt = result.expiresAt
             
             // Update cookies with the new tokens and expiration time
             setAuthCookies({
@@ -35,10 +43,43 @@ const refreshToken = async () => {
                 refreshToken: newRefreshToken,
                 expiresAt: newExpiresAt,
             })
-          })
-          .catch((error) => {
-            console.error('Mutation Error:', error)
-          })
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+        // Alternate method I attempted using GQL that was causing infinite loading.
+        // client.mutate(
+        //     {
+        //         mutation: gql`
+        //         mutation RefreshSession($accessToken: String!, $refreshToken: String!) {
+        //             refreshSession(accessToken: $accessToken, refreshToken: $refreshToken) {
+        //               accessToken
+        //               refreshToken
+        //               expiresAt
+        //             }
+        //           }
+        //           `,
+        //         variables: {
+        //             accessToken: accessToken,
+        //             refreshToken: refreshToken,
+        //         }
+        //     }
+        // ).then((result) => {
+        //     newAccessToken = result.data.refreshSession.accessToken
+        //     const newRefreshToken = result.data.refreshSession.refreshToken
+        //     const newExpiresAt = result.data.refreshSession.expiresAt
+            
+        //     // Update cookies with the new tokens and expiration time
+        //     setAuthCookies({
+        //         accessToken: newAccessToken,
+        //         refreshToken: newRefreshToken,
+        //         expiresAt: newExpiresAt,
+        //     })
+        //   })
+        //   .catch((error) => {
+        //     console.error('Mutation Error:', error)
+        //   })
 
 
         return newAccessToken
@@ -61,30 +102,37 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
-let processingTokenRefresh = false
 // Link for handling session refresh and retrying the initial request via forward
 const errorLink = onError(({ graphQLErrors, operation, forward }) => {
     // Since "SESSION_EXPIRED" is what the error returned is, refresh token in this case
     const sessionExpiredError = graphQLErrors?.find(error => error.extensions.code === "SESSION_EXPIRED")
 
-    if (sessionExpiredError && !processingTokenRefresh) {
-        processingTokenRefresh = true
-        refreshToken().then((accessToken) => {
-            operation.setContext({
-                headers: {
-                    ...operation.getContext().headers,
-                    authorization: `Bearer ${accessToken}`
+    if (sessionExpiredError) {
+        return new Observable((observer) => {
+            refreshToken()
+              .then((newAccessToken) => {
+                // If refresh was successful, update the headers and retry the operation
+                operation.setContext(({ headers = {} }) => ({
+                  headers: {
+                    ...headers,
+                    Authorization: `Bearer ${newAccessToken}`,
+                  },
+                }))
+                const subscriber = {
+                  next: observer.next.bind(observer),
+                  error: observer.error.bind(observer),
+                  complete: observer.complete.bind(observer),
                 }
-            })
-            processingTokenRefresh = false
-        })
-
-        return forward(operation)
+                forward(operation).subscribe(subscriber)
+              })
+              .catch((refreshError) => {
+                // If token refresh fails, return the original error
+                observer.error(refreshError)
+              })
+          })
     }
-
-    return forward(operation)
 })
-
+  
 export const client = new ApolloClient({
     link: ApolloLink.from([authLink, errorLink, httpLink]),
     cache: new InMemoryCache(),
--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,12 @@ async function main() {
     console.info('Server starting...')
     const graphQLServer = await NewGraphQLServer({ stores })
     graphQLServer.applyMiddleware({ path: '/graphql', app })
-
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        next()
+      })
     const restServer = NewRestServer({ stores })
     app.use('/api', restServer)
 
